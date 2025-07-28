29a4e0ed9f7c80d0b2568526df41f599a1682d89
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2025-07-29 11:21:17 +1200

    loaded env at top. added cloudy to advice call

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template
-from route_logic.uv_service import get_uv_data
-from route_logic.advice import get_clothing_advice
-
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Flask(__name__)
+from flask import Flask, render_template
+from route_logic.uv_service import get_uv_data
+from route_logic.advice import get_clothing_advice
+from route_logic.weather_service import is_cloudy
 
+app = Flask(__name__)
 
 @app.route("/")
 def index():
 	uv_max = get_uv_data()
+	cloudy = is_cloudy()
+
 	if uv_max is None:
 		advice = "Could not fetch UV data. Please try again later."
+	elif cloudy is None:
+		advice = "Could not fetch weather data. Please try again later."
 	else:
-		advice = get_clothing_advice(uv_max)
+		advice = get_clothing_advice(uv_max, is_cloudy=cloudy)
 
-	return render_template(
-			"index.html", uv_index=uv_max, advice=advice
-	)
+	return render_template("index.html", uv_index=uv_max, advice=advice,
+	                       cloudy=cloudy)
 
 
 if __name__ == "__main__":
