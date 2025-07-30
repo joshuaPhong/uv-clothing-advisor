f0ede22cc2602560fc8bf2c0bab821e3fe020d77
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2025-07-30 12:31:02 +1200

    creating the app

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+# app/__init__.py
+
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def create_app():
+	app = Flask(__name__)
+	app.secret_key = os.getenv('SECRET_KEY', 'fallback-secret-key')
+
+	from .routes import main_bp
+	app.register_blueprint(main_bp)
+
+	return app
